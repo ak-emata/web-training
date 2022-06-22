acd1245217d065325c64c69e835c5f5557f50cce
Esteban Mata <emata@akurey.com>
2022-06-22 14:01:33 -0600

    [Task9][feat] fixed navbar styling and added searchbar

--- task09-react/src/App.js
+++ task09-react/src/App.js
@@ -2,12 +2,14 @@ import "./App.scss";
 
 import CustomNavbar from "./components/customNavbar";
 import Sipnosis from "./components/sipnosis";
+import Searchbar from "./components/searchbar";
 
 function App() {
   return (
     <>
       <CustomNavbar />
       <Sipnosis />
+      <Searchbar />
     </>
   );
 }
